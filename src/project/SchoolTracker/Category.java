6542c1c8faa6e2134eab302a7fe2bfcfcc00dde9
Alexander <Alexander@10.68.251.95>
2017-04-11 19:25:40 -0500

    you know

---
+++ src/project/SchoolTracker/Category.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.SchoolTracker;
+
+/**
+ * This class represents a category of grades for a course
+ * @author Alexander
+ */
+import java.util.ArrayList;
+
+public class Category 
+{
+    private double weight;
+    private String name;
+    private double average;
+    private ArrayList assignments;
+    /**
+     * Constructor initializes instance variables
+     * @param name the name of the category
+     * @param weight the weight that this category has in calculating the grade for the course
+     */
+    public Category(String name, double weight)
+    {
+        assignments = new ArrayList<Assignment>();
+        this.name = name;
+        this.weight = weight;
+        average = Calculator.calculateAverage(assignments);
+    }
+    
+    /**
+     * returns the average grade for the category
+     * @return 
+     */
+    public double getAverage()
+    {
+        return average;
+    }
+    
+    /**
+     * returns the categories weight
+     * @return weight the weight of the category
+     */
+    public double getWeight()
+    {
+        return weight;
+    }
+    
+    /**
+     * returns the name of the category
+     * @return name the name of the category
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    /** 
+     * changes the name of the category
+     * @param newName the new name of the category
+     */
+    public void setName(String newName)
+    {
+        this.name = newName;
+    }
+    
+    /**
+     * changes the weight of category
+     * @param newWeight the new weight of the category
+     */
+    public void setWeight(double newWeight)
+    {
+        this.weight = newWeight;
+    }
+    /**
+     * calculates the average of the assignments in the category
+     */
+    public void calculateAverage()
+    {
+        average = Calculator.calculateAverage(assignments);
+    }
+    /**
+     * adds an assignment to the category and calculates the new average
+     * @param newAssignment the assignment being added to the category
+     */
+    public void addAssignment(Assignment newAssignment)
+    {
+        assignments.add(newAssignment);
+        calculateAverage();
+    }
+    
+    public String toString()
+    {
+        String result = name + " Weight: " +  weight;
+    }
+}
---
+++ src/project/SchoolTracker/Student.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.SchoolTracker;
+
+/**
+ * This class creates the student
+ * @author Kyle
+ * @author Alex
+ * @version 3
+ */
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Student implements Serializable
+{
+    private double gpa;
+    private int creditsNeeded;
+    private String name;
+    private int id;
+    private int creditsEarned;
+    private ArrayList courses;
+    
+    /**
+     * Constructor to initialize instance variables 
+     * @param name name of the student
+     * @param id number id for student
+     * @param creditsNeeded the amount of credits the student needs to graduate
+     */
+    public Student(String name, int id, int creditsNeeded)
+    {
+        this.name = name;
+        this.id = id;
+        this.creditsNeeded = creditsNeeded;
+        courses = new ArrayList<Course>(); 
+        gpa = Calculator.calculateGPA(courses);
+        creditsEarned = Calculator.calculateCreditsEarned(courses); 
+    }
+    
+    /**
+     * returns the gpa of the student
+     * @return gpa the gpa of the student 
+     */
+    public double getGpa()
+    {
+        return gpa;
+    }
+    
+    /**
+     * returns the number of credits needed to graduate
+     * @return creditsNeeded the number of credits needed to graduate
+     */
+    public int getCreditsNeeded()
+    {
+        return creditsNeeded;
+    }
+    
+    /**
+     * returns the name of the student
+     * @return name the name of the student
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    /**
+     * returns the id of the student
+     * @return id the id number of the student
+     */
+    public int getId()
+    {
+        return id;
+    }
+    
+    /**
+     * returns the number of the credits earned by the student
+     * @return creditsEarned the number of credits earned
+     */
+    public int getCreditsEarned()
+    {
+        return creditsEarned;
+    }
+    
+    /**
+     * changes the students name
+     * @param studentName the new name of the student
+     */
+    public void setName(String studentName)
+    {
+        name = studentName;
+    }
+    
+    /**
+     * changes the students id 
+     * @param studentId the new number id of the student 
+     */
+    public void setId(int studentId)
+    {
+        id = studentId;
+    }
+    
+    /**
+     * changes the number of credits needed to graduate
+     * @param needed 
+     */
+    public void setCreditsNeeded(int needed)
+    {
+        creditsNeeded = needed;
+    }
+    /**
+     * calculates the gpa of the student
+     */
+    public void calculateGPA()
+    {
+        gpa = Calculator.calculateGPA(courses);
+    }
+    /**
+     * calculates the number of credits earned by the student
+     */
+    public void calculateCreditsEarned()
+    {
+        creditsEarned = Calculator.calculateCreditsEarned(courses);
+    }
+    
+    /**
+     * adds a course to students ArrayList of Courses
+     * @param newCourse the course being added
+     */
+    public void addCourse(Course newCourse)
+    {
+        courses.add(newCourse);
+    }
+    
+    /**
+     * String conversion for the student
+     * @return the string showing name gpa and credits earned
+     */
+    public String toString()
+    {
+        return name + " GPA: " + gpa + " CreditsEarned:  " + creditsEarned;
+    }
+}
